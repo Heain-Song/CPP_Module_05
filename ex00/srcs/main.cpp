1b1da80e55cef5bb3db60d3a837ae28fef9a1933
Hea In Song <heain.song328@gmail.com>
2024-06-06 16:00:31 +0200

    WIP

---
+++ ex00/srcs/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hesong <hesong@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 05:04:36 by hesong            #+#    #+#             */
+/*   Updated: 2024/06/06 15:49:13 by hesong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+/**
+ * Design an artificial nightmare of offices, corridors, forms, and waiting queues.(???)
+ * Start by the smallest cog in this vast bureaucratic machine: the Bureaucrat. (????)
+ * A Bureaucrat must have a constant name, a grade from 1 to 150. 1 is the highest possible grade. (V)
+ * Throw an exception (Bureaucrat::GradeTooHighException OR Bureaucrat::GradeTooLowException) on any attempt to instantiate a Bureaucrat using an invalid grade.
+ * Implement getName() and getGrade()
+ * Implement two member functions to increment or decrement the bureaucrat grade
+ * If the grade is out of range, both of them will throw the same exceptions as the constructor.
+ * Implement an overload of the insertion («) operator to print something like (without the angle brackets): <name>, bureaucrat grade <grade>
+ * Turn in some tests to prove everything works as expected.
+ */
+
+int main(void)
+{
+
+
+}
+
